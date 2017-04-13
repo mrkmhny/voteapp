7be60f2bd01309bbcfcbd69259aa364313b71738
terebijoke <Mark@Marks-MacBook-Pro.local>
2017-04-13 01:19:42 -0400

    poll editor design and first field binding

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import bg from './bg.png';
 import './App.css';
 import Chart from 'chart.js';
 import {FieldGroup, FormGroup, FormControl, Radio, ControlLabel,
         Button, Navbar, Nav, NavItem, NavDropdown, 
-        MenuItem, Glyphicon, Dropdown} from 'react-bootstrap';
+        MenuItem, Glyphicon, Dropdown, Well, InputGroup} from 'react-bootstrap';
 import fetch from 'fetch';
 import $ from 'jquery';
+import Sortable from 'sortablejs';
+import update from 'immutability-helper';
 
 //import mongoose from '../../node_modules/mongoose';
 //import Data from '../../models.js';
@@ -16,9 +17,35 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      data: [12, 19, 3, 5, 2, 3]
+      data: [12, 19, 3, 5, 2, 3],
+
+      newPoll:{
+        title: 'test title',
+        choices:[12, 'A', 3, 5, 2, 3]
+      },
+
+      polls:[
+        {
+          title:'test title',
+          choices:[
+            {
+              name:'testchoice1'
+            }
+          ]
+
+        }
+        ]
+      }
+      this.handleInput = this.handleInput.bind(this);
     };
+  
+  handleInput = (e) => {
+    this.setState(update(this.state,{
+      newPoll:{title:{$set:e.target.value}}
+    }))
   }
+
+  
   
   componentDidMount(){
     /*
@@ -30,6 +57,7 @@ class App extends Component {
       }
     });
 */
+
   }
   
   
@@ -37,7 +65,52 @@ class App extends Component {
     return (
       <div className="App">
 
-        <div className="App-header">
+        <AppHeader />
+        
+        <br/>
+        <div className="App-body">
+          <div className="CreatePoll">
+            <h2 style={{textAlign:'center'}}>Create a new poll by using the editor below!</h2>
+
+            <div className="editor" style={{display:'flex', justifyContent:'space-around'}}>
+              <PollCreator 
+                data={this.state.data}
+                newPoll={this.state.newPoll}
+                handleInput={this.handleInput}
+              />
+              <div className="preview" style={{width:"20%",textAlign:'center'}}>
+                <h3>Preview</h3>
+
+                <h5>This is the title</h5>
+                <Poll data={this.state.data}
+                    newPoll={this.state.newPoll}
+                    handleInput={this.state.handleInput}/>
+              </div>
+            </div>
+          </div>
+          
+          
+          
+          
+          <div className="Poll">
+            <div style={{display:'none', justifyContent:'space-around'}}>
+              <Poll data={this.state.data}
+                    newPoll={this.state.newPoll}
+                    handleInput={this.state.handleInput}/>
+              <Graph data={this.state.data}/>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+class AppHeader extends Component {
+  render(){
+    return(
+      <div className="App-header">
           
           <Nav pullRight bsStyle="pills" style={{display:'flex'}}>
                 <a href="#" style={{color:'white', margin:'10px'}}>Create Poll</a>
@@ -58,7 +131,6 @@ class App extends Component {
               
             </div>
           </div>
-        </div>
         {/*
           end of app header
           eventually, what we'll do is put this in it's own AppHeader component,
@@ -66,91 +138,100 @@ class App extends Component {
             - dynamically update Sign up / login in to say the user's name
             - i think that's it
         */}
-        <br/>
-        <div className="App-body">
-          <div className="CreatePoll">
-            
-            <h2 style={{textAlign:'center'}}>Create a new poll by using the editor below!</h2>
 
-            <div className="editor" style={{display:'flex', justifyContent:'space-around'}}>
-              <div className="options" style={{width:"20%",textAlign:'center'}}>
-              <form>
-                <FormGroup>
-                  <h3>Poll Editor</h3>
-                  <ControlLabel>Poll Text</ControlLabel>
-                  <FormControl
-                    componentClass="textarea"
-                    //value=this.state.value
-                    placeholder="Enter text"
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-                <ControlLabel>Choices</ControlLabel>
-                <FormGroup>
-                  <FormControl
-                    type="text"
-                    //value=this.state.value
-                    placeholder="Enter text"
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <FormControl
-                    type="text"
-                    //value=this.state.value
-                    placeholder="Enter text"
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <FormControl
-                    type="text"
-                    //value=this.state.value
-                    placeholder="Enter text"
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-              </form>
-              </div>
-              <div className="preview" style={{width:"20%",textAlign:'center'}}>
-                <h3>Preview</h3>
+      </div>
 
-                <h5>This is the title</h5>
-                <MultChoice data={this.state.data}/>
-              </div>
-            </div>
-          </div>
-          
-          
-          
-          
-          <div className="Poll">
-            <div style={{display:'none', justifyContent:'space-around'}}>
-              <MultChoice data={this.state.data}/>
-              <Graph data={this.state.data}/>
-            </div>
-          </div>
-        </div>
-        
+      
+    );
+  }
+}
+
+class PollCreator extends Component {
+  
+  componentDidMount(){
+    var el = document.getElementById('choices-editor');
+    var sortable = new Sortable(el,{
+      animation:150,
+      handle:".drag-handle"});
+  }
+  
+  render(){
+    return(
+      <div className="inputs-left" style={{width:"60%"}}>
+        <Well style={{background:'antiqueWhite'}}>
+          <form>
+            <FormGroup>
+              <h3>Poll Editor</h3>
+              <ControlLabel>Poll Text</ControlLabel>
+              <FormControl
+                componentClass="textarea"
+                onChange={this.props.handleInput}
+                value={this.props.newPoll.title}
+                placeholder="Enter text"
+              />
+            </FormGroup>
+            <ControlLabel>Choices</ControlLabel>
+            <FormGroup id="choices-editor" style={{textAlign:'right'}}>
+              <EditChoice />
+              <EditChoice />
+              <EditChoice />
+              <Button style={{background:'whiteSmoke'}}>
+                <Glyphicon glyph="plus" bsStyle="secondary" style={{color:'lightGreen'}}/>
+              </Button>
+            </FormGroup>
+          </form>
+          <FormGroup style={{textAlign:"center"}}>
+            <Button bsStyle="primary" style={{marginRight:'15px'}}>Submit</Button>
+            <Button bsStyle="default">Clear</Button>
+          </FormGroup>
+        </Well>
       </div>
     );
   }
 }
 
-class MultChoice extends Component {
+class EditChoice extends Component {
+  render(){
+    return(
+      <FormGroup>
+        <InputGroup>
+          <FormControl
+            type="text"
+            //value=this.state.value
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <InputGroup.Addon className="drag-handle">
+            <Glyphicon glyph="menu-hamburger"/>
+          </InputGroup.Addon>
+          <InputGroup.Addon>
+            <Glyphicon glyph="remove" style={{color:'red'}}/>
+          </InputGroup.Addon>
+        </InputGroup>
+      </FormGroup>
+    );
+  }
+}
+
+
+class Poll extends Component {
   render() {
-    
+    /*
     var data = {};
     data.radios = this.props.data.map(function(c,i,a){
       return <Radio key={i} name="choices">{c}</Radio>;
     });
-  
+    */
+    var pollTitle = this.props.newPoll.title;
+    var pollRadios = this.props.newPoll.choices.map(function(c,i,a){
+      return <Radio key={i} name="choices">{c}</Radio>;
+    });
     
     return (
       <div>
         <form action="/api" method="post">
-          <ControlLabel>Choose one</ControlLabel>
-          {data.radios}
+          <ControlLabel>{pollTitle}</ControlLabel>
+          {pollRadios}
           <Button type="submit" bsStyle="primary">Submit</Button>
         </form>
       </div>
